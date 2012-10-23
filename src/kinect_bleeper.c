81134d994764f892cc3a64c82de264a15975cfe5
Rafael Lorandi <coolparadox@gmail.com>
2012-10-23 01:38:03 -0200

    License statement in source file(s).

--- src/kinect_bleeper.c
+++ src/kinect_bleeper.c
@@ -3,6 +3,11 @@
  *
  * Copyright (c) 2012 coolparadox.com
  *
+ * This code is licenced according to the GNU General Public Licence version 3.
+ * A file COPYING is distributed with the sources of this project containing
+ * the full text of the license. Optionally, the full terms of the license 
+ * can also be downloaded from http://www.gnu.org/licenses/gpl.html .
+ *
  */
 
 #include <config.h>
