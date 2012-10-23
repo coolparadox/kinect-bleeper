802e834b078b22fb4e33691b0982317e07c3277d
Rafael Lorandi <coolparadox@gmail.com>
2012-10-23 01:22:48 -0200

    Initialization and shutdown of kinetic depth stream.

--- src/kinect_bleeper.c
+++ src/kinect_bleeper.c
@@ -1,5 +1,8 @@
-/* kinect_bleeper - obstacle avoidance using sounds and openkinect
- * by Rafael Lorandi <coolparadox@gmail.com>
+/*
+ * kinect_bleeper - obstacle avoidance using sounds and openkinect
+ *
+ * Copyright (c) 2012 coolparadox.com
+ *
  */
 
 #include <config.h>
@@ -7,6 +10,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <argp.h>
+#include <signal.h>
+
+#include <libfreenect/libfreenect.h>
+
+volatile sig_atomic_t running = 1;
 
 /*
  * Command line argument parsing functions.
@@ -50,6 +58,16 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state) {
 
 static struct argp argp = { options, parse_opt, args_doc, doc };
 
+void process_depth(freenect_device *dev, void *depth, uint32_t timestamp);
+
+void signal_cleanup(int num) {
+
+	running = 0;
+	fprintf(stderr, "\ncleaning up...\n");
+	signal(SIGINT, signal_cleanup);
+
+}
+
 /*
  * The portal.
  */
@@ -58,15 +76,58 @@ int main (int argc, char **argv) {
 
 	struct arguments arguments;
 
+	freenect_context *ctx;
+	freenect_device *dev;
+
 	/* Get CLI arguments. */
         memset (&arguments, 0, sizeof (struct arguments));
         argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
-	/* Sanity check CLI arguments. */
+	/* Cleanup on interruption. */
+	signal(SIGINT, signal_cleanup);
 
-	/* Under construction. */
-	fprintf (stderr, "error: this routine is not yet implemented.\n");
-	return (1);
+	/* Initialize kinetic device. */
+	if (freenect_init(&ctx, NULL)) {
+		fprintf(stderr, "error: cannot initialize freenect context.\n");
+		return (1);
+	}
+	freenect_select_subdevices(ctx, FREENECT_DEVICE_CAMERA);
+	if (freenect_open_device(ctx, &dev, 0)) {
+		fprintf(stderr, "error: cannot open kinect device.\n");
+		return (1);
+	}
+	if (freenect_set_depth_mode(dev, freenect_find_depth_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_11BIT))) {
+		fprintf(stderr, "error: cannot set depth mode.\n");
+		goto shutdown;
+	}
+	freenect_set_depth_callback(dev, process_depth);
+	if (freenect_start_depth(dev)) {
+		fprintf(stderr, "error: cannot start depth stream.\n");
+		goto shutdown;
+	}
+
+	/* Process frame events. */
+	while (running && freenect_process_events(ctx) >= 0);
+
+shutdown:
+
+	/* Shutdown kinetic device. */
+	if (freenect_stop_depth(dev)) {
+		fprintf(stderr, "warning: cannot stop depth stream.\n");
+	}
+	if (freenect_close_device(dev)) {
+		fprintf(stderr, "warning: cannot close freenect device.\n");
+	}
+	if (freenect_shutdown(ctx)) {
+		fprintf(stderr, "warning: cannot shutdown freenect context.\n");
+	}
+	
+}
+
+void process_depth(freenect_device *dev, void *depth, uint32_t timestamp) {
+
+	/* FIXME: implement depth stream processing here. */
+	printf ("%u\n", timestamp);
 
 }
 
